f5c35fb37956899eff873fcbeb54ab6827f46888
DaveKeith <d.keith78@yahoo.com>
2016-04-12 13:52:53 -0400

    added a few methods to the pct and cct (acronymns)

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,5 +1,6 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionController::TestCase
-
+  test "a comment can be added to a post" do
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,5 +1,13 @@
 require "test_helper"
 
 class PostsControllerTest < ActionController::TestCase
+  test "a post can be added" do
+  end
 
+  test "a post can be updated" do
+
+  end
+
+  test "a post can be deleted" do
+  end
 end
