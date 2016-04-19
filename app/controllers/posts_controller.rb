57e98cd4236ed3dc9c08b78d8d0f6a1e6d7f86f3
DaveKeith <d.keith78@yahoo.com>
2016-04-19 13:33:15 -0400

    changed the index design to something more simple and functional.  Added working pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts = Post.page(params[:page]).per(10)
     render :index, locals: { posts: @posts }
   end
 
